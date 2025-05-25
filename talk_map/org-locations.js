31e41496729ceeb7ca159fafdfcbbde62b85c76c
brianjbao <brianjbao@gmail.com>
2025-05-25 16:00:53 -0400

    Update org-locations.js

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -2,7 +2,7 @@ var addressPoints = [
   [
     "Richmond, VA, USA | Pauley Heart Center Symposium | 2023 | Hydrogen Sulfide Attenuates Doxorubicin-induced Exacerbation of Myocardial Ischemia-Reperfusion Injury in Mice",
     37.5407,
-    77.4360
+    -77.4360
   ],
   [
     "Atlanta, GA, USA | American Public Health Association Annual Meeting | 2023 | Novel substance use and support themes on Reddit: Lessons for researchers and practitioners",
