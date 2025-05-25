38a6ec3e73e906a29adba51d9ac74bdf0f3d7f98
brianjbao <brianjbao@gmail.com>
2025-05-25 16:15:03 -0400

    Update org-locations.js

--- talk_map/org-locations.js
+++ talk_map/org-locations.js
@@ -1,12 +1,37 @@
 var addressPoints = [
   [
     "Richmond, VA, USA | Pauley Heart Center Symposium | 2023 | Hydrogen Sulfide Attenuates Doxorubicin-induced Exacerbation of Myocardial Ischemia-Reperfusion Injury in Mice",
-    37.5407,
-    -77.4360
+    37.541069,
+    -77.430458
   ],
   [
-    "Atlanta, GA, USA | American Public Health Association Annual Meeting | 2023 | Novel substance use and support themes on Reddit: Lessons for researchers and practitioners",
-    33.7489924,
-    -84.3902644
+    "Richmond, VA, USA | Pauley Heart Center Symposium | 2023 | NLRP3 inflammasome inhibitor attenuates cisplatin-induced macrophage foam cell death in an in vitro model of atherosclerosis",
+    37.541069,
+    -77.430458
+  ],
+  [
+    "Prague, Czech Republic | International Society for Heart and Lung Transplantation 44th Annual Meeting | 2024 | Combined Lung-Liver Transplant: Recipient Risk Factors",
+    50.055870,
+    14.436840
+  ],
+  [
+    "Boston, MA, USA | International Society for Heart and Lung Transplantation 45th Annual Meeting | 2025 | Impact of Functional Warm Ischemia Time on Donation After Circulatory Death Heart Transplant",
+    42.347530,
+    -71.084106
+  ],
+  [
+    "Boston, MA, USA | International Society for Heart and Lung Transplantation 45th Annual Meeting | 2025 | The Utilization and Outcomes of Hypoxemic Donors in Combined Heart-Lung Transplantation",
+    42.347530,
+    -71.084106
+  ],
+  [
+    "Boston, MA, USA | International Society for Heart and Lung Transplantation 45th Annual Meeting | 2025 | Long-Term Utilization, Experience, and Outcomes of COVID-19 Positive Donors for Heart Transplantation",
+    42.347530,
+    -71.084106
+  ],
+  [
+    "Boston, MA, USA | International Society for Heart and Lung Transplantation 45th Annual Meeting | 2025 | Optimization of Preoperative Amiodarone Therapy for Survival and Primary Graft Dysfunction in Patients undergoing Heart Transplantation",
+    42.347530,
+    -71.084106
   ]
 ];
